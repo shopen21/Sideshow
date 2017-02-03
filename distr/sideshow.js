63b00015c225f3e254980b87f36b2858db0a0493
Andriy Shafran <shopen21@gmail.com>
2017-02-04 02:06:58 +0300

    test commit

--- distr/sideshow.js
+++ distr/sideshow.js
@@ -5,6 +5,7 @@
  Date: 2015-03-19
  Author: Fortes Informática http://www2.fortesinformatica.com.br/
  Available under Apache License 2.0 (https://raw2.github.com/fortesinformatica/sideshow/master/LICENSE)
+ Modified by shopen21 :)
  **/
 
 ;
